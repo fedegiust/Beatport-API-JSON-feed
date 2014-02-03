587e79e5f76f41198a7129a56c7b22276c37f772
Federico Giust <fgiustdj@gmail.com>
2014-02-03 12:48:16 -0200

    Log api calls

--- log_calls.php
+++ log_calls.php
@@ -6,16 +6,16 @@ try{
 	$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	// $db->exec("CREATE TABLE IF NOT EXISTS `api_calls` (
-	// 			`id` integer AUTO_INCREMENT NOT NULL,
-	// 			`http_referer` varchar(255) NOT NULL,
-	// 			`query_string` varchar(255),
-	// 			`browser` varchar(255),
-	// 			`ip` varchar(15),
-	// 			`time_stamp` varchar(255),
-	// 			PRIMARY KEY (`id`)) 
-	// 			CHARACTER SET utf8 COLLATE utf8_general_ci;") or die(print_r($db->errorInfo(), true));
-	//echo " DB Created Success";
+	$db->exec("CREATE TABLE IF NOT EXISTS `api_calls` (
+				`id` integer AUTO_INCREMENT NOT NULL,
+				`http_referer` varchar(255) NOT NULL,
+				`query_string` varchar(255),
+				`browser` varchar(255),
+				`ip` varchar(15),
+				`time_stamp` varchar(255),
+				PRIMARY KEY (`id`)) 
+				CHARACTER SET utf8 COLLATE utf8_general_ci;") or die(print_r($db->errorInfo(), true));
+	echo " DB Created Success";
 
 	$stmt = $db->prepare("INSERT INTO api_calls (http_referer, query_string, browser, ip, time_stamp) VALUES (?,?,?,?,?);");
 
